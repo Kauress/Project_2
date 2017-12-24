c095ca35af69f2566248766d865aafe5998c0db1
Kauress <kauress8@gmail.com>
2017-12-24 23:08:05 +0530

    Update shop.js

---
+++ js/shop.js
@@ -0,0 +1,58 @@
+// products is an array containing JSON objects
+var products = [{
+  "Name": "Happy Birthday",
+  "Description": "Birthday greeting",
+  "Price": 2.99,
+  "Image": "images/happybirthday.jpg",
+  "Category": "Greetings"
+}, {
+  "Name": "Design Things",
+  "Description": "Design card",
+  "Price": 3.99,
+  "Image": "images/designthings.jpg",
+  "Category": "Design"
+
+}, {
+  "Name": "Brain Acrobat",
+  "Description": "Synaptic specialists",
+  "Price": 4.99,
+  "Image": "images/brainacrobat.jpg",
+  "Category": "Speciality"
+}, {
+  "Name": "Design Experts",
+  "Description": "Unicorn design experts",
+  "Price": 3.99,
+  "Image": "images/unicorndesign.jpg",
+  "Category": "Design"
+}, {
+  "Name": "Sun Tzu",
+  "Description": "The art of the DOM",
+  "Price": 6.99,
+  "Image": "images/suntzu.jpg",
+  "Category": "Speciality"
+}, {
+  "Name": "Dogs",
+  "Description": "Dogs of the world unite",
+  "Price": 4.99,
+  "Image": "images/dogsoftheworld.jpg",
+  "Category": "Greetings"
+}, {
+  "Name": "Code",
+  "Description": "Code this",
+  "Price": 5.99,
+  "Image": "images/code.jpg",
+  "Category": "Code"
+}, {
+  "Name": "Fi Dren",
+  "Description": "The wifi guy",
+  "Price": 8.99,
+  "Image": "images/fidren.jpg",
+  "Category": "Speciality"
+}, {
+  "Name": "Bugs Studio",
+  "Description": "Fixing developers",
+  "Price": 9.99,
+  "Image": "images/bugstudio.jpg",
+  "Category": "Code"
+}
+]; // end of products array
