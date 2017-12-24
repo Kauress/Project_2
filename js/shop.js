db3ff092bcd75879f20db3a1ee3879b7d27e27fc
Kauress <kauress8@gmail.com>
2017-12-24 23:10:16 +0530

    Update shop.js

--- js/shop.js
+++ js/shop.js
@@ -1,3 +1,8 @@
-// Window onload function build the DOM
-window.onload = buildDOM();
 
+// function list
+function list() {
+    var displayItems = document.getElementsByClassName("product-div");
+    for(var i = 0; i < displayItems.length; i++) {
+      displayItems[i].style.width = "100%";
+    }// end of for
+}// end of function
