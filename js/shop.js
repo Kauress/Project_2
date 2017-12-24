2b5e61f990fe7b4ce252f6a4e89c7700049b825b
Kauress <kauress8@gmail.com>
2017-12-24 23:12:04 +0530

    Update shop.js

--- js/shop.js
+++ js/shop.js
@@ -1,13 +1,20 @@
-// onchange function sortByChoice
-document.getElementById("sortByChoice").onchange = function() {
-    var selectElement = document.getElementById("sortByChoice");
-    var selectedOption = selectElement.options[selectElement.selectedIndex].value;
-    if(selectedOption === "LowToHigh") {
-      products.sort(ascendingOrder);
-    } else if(selectedOption === "HighToLow") {
-      products.sort(descendingOrder);
-    } else {
-      products.sort(alphabeticalOrder);
-    }
-    buildDOM();
-}; // end of onchange function
+// function ascendingOrder
+function ascendingOrder(a,b) {
+    for(var i = 0; i < products.length; i++) {
+       return (a.Price) - (b.Price);
+    }// end of for
+}// end of function
+
+// function descendingOrder
+function descendingOrder(a,b) {
+    for(var i = 0; i < products.length; i++) {
+        return (b.Price) - (a.Price);
+    }// end of for
+}// end of function
+
+// function alphabeticalOrder
+function alphabeticalOrder(a,b) {
+    for(var i = 0; i < products.length; i++) {
+        return (a.Name) > (b.Name);
+    }// end of for
+}// end of function
