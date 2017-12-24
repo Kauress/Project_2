b8d407c6f112c28aa9075b97253a46624f51fc5f
Kauress <kauress8@gmail.com>
2017-12-24 23:14:06 +0530

    Update shop.js

--- js/shop.js
+++ js/shop.js
@@ -1,11 +1,27 @@
-// Cart object
-var cart = {
-items: [],
-numberItems: 0,
-total: 0,
-coupon: ["50%Off", "30%Off"],
-wishlist: [],
-couponValue: 1,
-shipping: 0,
-shippingLocation: ""
-};
+// function getProductInfo
+function getProductInfo(index) {
+  var quantity = parseInt(document.getElementById("product-qty" + index).value);
+  //alert(quantity);
+  var product = {
+    Id: index,
+    Name: products[index].Name,
+    Description: products[index].Description,
+    Price: products[index].Price,
+    Amount: products[index].Price * quantity,
+    Quantity: quantity
+  }; // end of object
+  //alert(product.Id);
+  var productAlreadyInCart = false;
+  for(var i = 0; i < cart.items.length; i++) {
+    if(cart.items[i].Id === index) {
+      productAlreadyInCart = true;
+      cart.items[i].Amount = (product.Amount + cart.items[i].Amount);
+      cart.items[i].Quantity = product.Quantity + cart.items[i].Quantity;
+      break;
+    }// end of if
+  }// end of for
+  if(!productAlreadyInCart) {
+    cart.items.push(product);
+  }// end of if
+  buildCart();
+}// end of function
