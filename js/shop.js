529af5b6f135b212526a532b7b4b364a7b62ce66
Kauress <kauress8@gmail.com>
2017-12-24 23:18:01 +0530

    Update shop.js

--- js/shop.js
+++ js/shop.js
@@ -1,13 +1,4 @@
-
-//function coupon
-function coupon() {
-    var couponInput = document.getElementById("couponCode");
-    if(couponInput.value === "30%Off") {
-        cart.couponValue = 0.30;
-    } else if(couponInput.value === "50%Off") {
-       cart.couponValue = 0.50;
-    } else {
-      cart.couponValue = 1;
-    }
-    productTotal();
+// function couponInputChange
+function couponInputChange() {
+    document.getElementById("couponCode").value = "Discount applied";
 }// end of function
