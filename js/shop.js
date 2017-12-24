a63043386ae06f5323f68d4bae0ad8b9fcdd090c
Kauress <kauress8@gmail.com>
2017-12-24 23:20:31 +0530

    Update shop.js

--- js/shop.js
+++ js/shop.js
@@ -1,26 +1,6 @@
-//function shipping
-function shipping() {
-    var selected = document.getElementById("selectBox").value;
-    if(selected === "Please select a country") {
-      document.getElementById("shippingRate").innerHTML = "";
-      cart.shipping = 0;
-      cart.shippingLocation = "";
-    } else if(selected === "Canada") {
-      document.getElementById("shippingRate").innerHTML = "$" + 9.99;
-      cart.shipping = 9.99;
-      cart.shippingLocation = "Canada";
-    }  else if(selected === "China") {
-      document.getElementById("shippingRate").innerHTML = "$" + 10.99;
-      cart.shipping = 10.99;
-      cart.shippingLocation = "China";
-    } else if(selected === "Europe") {
-      document.getElementById("shippingRate").innerHTML = "$" + 19.99;
-      cart.shipping = 19.99;
-      cart.shippingLocation = "Europe";
-    } else {
-      document.getElementById("shippingRate").innerHTML = "$" + 9;
-      cart.shipping = 9;
-      cart.shippingLocation = "USA";
-    }
-    grandTotal();
-}//end of shipping
+// function showShipping
+function showShipping() {
+    if(cart.shipping !== 0) {
+        document.getElementById("selectBox").value = cart.shippingLocation;
+    }// end of if
+}// end of function
