36e3b9b663664da548e27d5846269b246f91394a
Kauress <kauress8@gmail.com>
2017-12-24 23:10:41 +0530

    Update shop.js

--- js/shop.js
+++ js/shop.js
@@ -1,8 +1,7 @@
-
-// function list
-function list() {
+// function grid
+function grid() {
     var displayItems = document.getElementsByClassName("product-div");
     for(var i = 0; i < displayItems.length; i++) {
-      displayItems[i].style.width = "100%";
+      displayItems[i].style.width = "";
     }// end of for
 }// end of function
