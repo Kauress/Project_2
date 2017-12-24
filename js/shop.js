6eadc4aeeb1f758f35ed19bea6d92d3bbe488a06
Kauress <kauress8@gmail.com>
2017-12-24 23:13:24 +0530

    Update shop.js

--- js/shop.js
+++ js/shop.js
@@ -1,28 +1,11 @@
-// function updateFilter
-function updateFilter() {
-    var category = [];
-    var inputCheckboxes = document.getElementsByClassName("filterCategory");
-    var products = document.getElementsByClassName("product-div");
-    for(var i = 0; i < inputCheckboxes.length; i++) {
-      if(inputCheckboxes[i].checked) {
-        var inputCheckboxValue = inputCheckboxes[i].getAttribute("value");
-        category.push(inputCheckboxValue);
-        //alert(category);
-      }// end of if
-    }// end of for
-    for(var j = 0; j < products.length; j++) {
-      var productCategory = products[j].getAttribute("category");
-      products[j].style.display = "none";
-      for(var k = 0; k < category.length; k++) {
-        if(category[k] === productCategory) {
-          products[j].style.display = "block";
-          break;
-        }// end of if
-      }// end of for
-    }// end of for
-    for(var l = 0; l < products.length; l++) {
-      if(category.length === 0) {
-          products[l].style.display = "block";
-      }// end of if
-    }// end of for
-}//end of function
+// Cart object
+var cart = {
+items: [],
+numberItems: 0,
+total: 0,
+coupon: ["50%Off", "30%Off"],
+wishlist: [],
+couponValue: 1,
+shipping: 0,
+shippingLocation: ""
+};
