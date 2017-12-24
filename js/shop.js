908b436b9ec527d6d90adccfd372afc98ba5936c
Kauress <kauress8@gmail.com>
2017-12-24 23:11:26 +0530

    Update shop.js

--- js/shop.js
+++ js/shop.js
@@ -1,7 +1,13 @@
-// function grid
-function grid() {
-    var displayItems = document.getElementsByClassName("product-div");
-    for(var i = 0; i < displayItems.length; i++) {
-      displayItems[i].style.width = "";
-    }// end of for
-}// end of function
+// onchange function sortByChoice
+document.getElementById("sortByChoice").onchange = function() {
+    var selectElement = document.getElementById("sortByChoice");
+    var selectedOption = selectElement.options[selectElement.selectedIndex].value;
+    if(selectedOption === "LowToHigh") {
+      products.sort(ascendingOrder);
+    } else if(selectedOption === "HighToLow") {
+      products.sort(descendingOrder);
+    } else {
+      products.sort(alphabeticalOrder);
+    }
+    buildDOM();
+}; // end of onchange function
