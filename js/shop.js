ab63ce79013e43d658609269a7220bbcb6343b94
Kauress <kauress8@gmail.com>
2017-12-24 23:19:54 +0530

    Update shop.js

--- js/shop.js
+++ js/shop.js
@@ -1,4 +1,26 @@
-// function topCartDisplay
-function topCartDisplay() {
-    document.getElementById("topCartDisplay").value = cart.numberItems;
-}// end of function
+//function shipping
+function shipping() {
+    var selected = document.getElementById("selectBox").value;
+    if(selected === "Please select a country") {
+      document.getElementById("shippingRate").innerHTML = "";
+      cart.shipping = 0;
+      cart.shippingLocation = "";
+    } else if(selected === "Canada") {
+      document.getElementById("shippingRate").innerHTML = "$" + 9.99;
+      cart.shipping = 9.99;
+      cart.shippingLocation = "Canada";
+    }  else if(selected === "China") {
+      document.getElementById("shippingRate").innerHTML = "$" + 10.99;
+      cart.shipping = 10.99;
+      cart.shippingLocation = "China";
+    } else if(selected === "Europe") {
+      document.getElementById("shippingRate").innerHTML = "$" + 19.99;
+      cart.shipping = 19.99;
+      cart.shippingLocation = "Europe";
+    } else {
+      document.getElementById("shippingRate").innerHTML = "$" + 9;
+      cart.shipping = 9;
+      cart.shippingLocation = "USA";
+    }
+    grandTotal();
+}//end of shipping
